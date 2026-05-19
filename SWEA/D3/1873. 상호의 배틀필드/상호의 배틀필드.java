ad330d9c1f388fa0505801d5193f65bb57bb5185
seoyeong-y <97073138+seoyeong-y@users.noreply.github.com>
2026-05-19 23:38:45 +0900

    [D3] Title: 상호의 배틀필드, Time: 106 ms, Memory: 28,288 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -4,9 +4,9 @@ import java.util.StringTokenizer;
 
 class Solution {
     static int h, w;
-    static char[][] map;
-    static int tankR, tankC;
-    static char dir;
+    static char[][] map; // 게임 맵
+    static int tankR, tankC; // 전차 위치
+    static char dir; // 전차 방향
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,7 +14,7 @@ class Solution {
 
         for (int test_case = 1; test_case <= tc; test_case++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            
+
             h = Integer.parseInt(st.nextToken());
             w = Integer.parseInt(st.nextToken());
 
@@ -26,6 +26,7 @@ class Solution {
                 for (int j = 0; j < w; j++) {
                     map[i][j] = line.charAt(j);
 
+                    // 전차 위치 및 방향
                     if (map[i][j] == '^' || map[i][j] == 'v' || map[i][j] == '<' || map[i][j] == '>') {
                         tankR = i;
                         tankC = j;
@@ -50,10 +51,12 @@ class Solution {
         }
     }
 
+    // 전차 이동
     static void move(char cmd) {
         int nr = tankR;
         int nc = tankC;
 
+        // 방향 변경 및 위치 계산
         if (cmd == 'U') {
             dir = '^';
             nr--;
@@ -68,39 +71,45 @@ class Solution {
             nc++;
         }
 
+        // 현재 방향 갱신
         map[tankR][tankC] = dir;
 
+        // 맵 밖은 이동 불가
         if (nr < 0 || nr >= h || nc < 0 || nc >= w) return;
 
+        // 평지인 경우 이동
         if (map[nr][nc] == '.') {
             map[tankR][tankC] = '.';
 
             tankR = nr;
             tankC = nc;
 
-            map[tankR][tankC] = dir;
+            map[tankR][tankC] = dir; // 이동한 위치에 전차 배치
         }
     }
 
+    // 포탄 발사
     static void shoot() {
         int dr = 0;
         int dc = 0;
 
+        // 현재 방향에 따른 이동 방향 설정
         if (dir == '^') dr = -1;
         else if (dir == 'v') dr = 1;
         else if (dir == '<') dc = -1;
         else if (dir == '>') dc = 1;
 
+        // 포탄 시작 위치
         int nr = tankR + dr;
         int nc = tankC + dc;
 
         while (nr >= 0 && nr < h && nc >= 0 && nc < w) {
-            if (map[nr][nc] == '*') {
+            if (map[nr][nc] == '*') { // 벽돌 벽이면 파괴 후 종료
                 map[nr][nc] = '.';
                 break;
             }
 
-            if (map[nr][nc] == '#') break;
+            if (map[nr][nc] == '#') break; // 강철 벽이면 종료
 
             nr += dr;
             nc += dc;
