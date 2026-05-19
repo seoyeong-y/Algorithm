274034f48b9bb551fb5b3f439dda5ed6d7a73d10
seoyeong-y <97073138+seoyeong-y@users.noreply.github.com>
2026-05-19 21:29:47 +0900

    [D3] Title: 농작물 수확하기, Time: 106 ms, Memory: 27,520 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Solution {
     public static void main(String args[]) throws Exception {
