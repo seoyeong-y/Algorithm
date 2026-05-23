f72584701bc44ee40e7ef8171c5bf8615a2bbac5
seoyeong-y <97073138+seoyeong-y@users.noreply.github.com>
2026-05-23 23:48:41 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 145 ms, Memory: 38,016 KB -BaekjoonHub

---
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Solution {
+    static String[] gns = {"ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"};
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int test_case = 1; test_case <= t; test_case++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String tc = st.nextToken();
+            int cnt = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            int[] arr = new int[10];
+
+            for (int i = 0; i < cnt; i++) {
+                String str = st.nextToken();
+                for (int j = 0; j < 10; j++) {
+                    if (str.equals(gns[j])) {
+                        arr[j]++;
+                        break;
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.append(tc);
+
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < arr[i]; j++) 
+                    sb.append(" ").append(gns[i]);
+            }
+
+            System.out.println(sb);
+        }
+    }
+}
