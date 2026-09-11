b3e7780c6510eabcebe484db8d8693d38f79bc3f
Yang Seoyeong <97073138+seoyeong-y@users.noreply.github.com>
2026-09-11 16:43:51 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -0,0 +1,26 @@
+def solution(a, b, c, d):
+    nums = [a, b, c, d]
+    counts = [nums.count(x) for x in nums]
+    
+    if max(counts) == 4:
+        return a * 1111
+    
+    elif max(counts) == 3:
+        p = nums[counts.index(3)]
+        q = nums[counts.index(1)]
+        return (10 * p + q) ** 2
+    
+    elif max(counts) == 2:
+        if min(counts) == 2:
+            if a == b:
+                return (a+c) * abs(a-c)
+            else:
+                return (a+b) * abs(a-b)
+        else:
+            p = nums[counts.index(2)]
+            return a*b*c*d // p**2
+        
+    else:
+        return min(nums)
+            
+            
