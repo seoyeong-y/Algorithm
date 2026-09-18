5a3c42f4f2027a45520bc0e846ad3789c847ca04
Yang Seoyeong <97073138+seoyeong-y@users.noreply.github.com>
2026-09-19 00:30:09 +0900

    [level 2] Title: 큰 수 만들기, Time: 44.27 ms, Memory: 14.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,13 @@
+def solution(number, k):
+    answer = []
+    
+    for num in number:
+        while answer and k > 0 and answer[-1] < num:
+            answer.pop()
+            k -= 1
+        answer.append(num)
+        
+    if k > 0:
+        answer = answer[:-k]
+        
+    return ''.join(answer)
