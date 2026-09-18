0b98fb1eae432dfb8ff509a043c1ffe1a7930aca
Yang Seoyeong <97073138+seoyeong-y@users.noreply.github.com>
2026-09-18 23:14:13 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,12 @@
+def solution(n, lost, reserve):
+    real_lost = sorted([l for l in lost if l not in reserve])
+    real_reserve = sorted([r for r in reserve if r not in lost])
+    
+    for r in real_reserve:
+        if r-1 in real_lost:
+            real_lost.remove(r-1)
+            
+        elif r+1 in real_lost:
+            real_lost.remove(r+1)
+        
+    return n - len(real_lost)
